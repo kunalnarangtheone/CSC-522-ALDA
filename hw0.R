1db46b205b2c7a44e0e4fd9a6a3795cede6738c9
Kunal Narang <knarang@ncsu.edu>
2019-01-20 15:41:39 -0500

    Intial commit

---
+++ hw0.R
@@ -0,0 +1,32 @@
+#######################
+# ALDA: hw0.R
+# Instructor: Dr. Thomas Price
+# Mention your team details here
+#
+#
+#
+#
+#########################
+require(ggplot2)
+set.seed(123)
+# no install.packages or rm(list=ls(all=T)) in this function
+
+
+intro_to_r <- function(num_values){
+  # input: num_values type: integer, specifies the sample size of the random vector you are to generate.
+  # output: a  list:  [generated vector (type: double), mean of vector (type: double), median of the vector (type: double), max value of vector (type: double), min value of the vector (type: double)]
+  
+  
+}
+
+intro_to_plotting <- function(num_values){
+  # input: num_values type: integer, specifies the sample size of the random vector you are to generate.
+  # output: two plots (saved to disk, no return value), descriptions for which have been provided in the hw0 document. 
+  
+  
+  
+}
+
+# do not call either function in this script 
+# this script is only for function definitions
+# if you wish to test your script, call your functions separately in a separate script.
